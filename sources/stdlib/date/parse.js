db5801a72dc229899aadae8e00ca671338f1ee4f
Maël Nison <nison.mael@gmail.com>
2017-11-18 15:36:52 +0000

    Removes the strict mode from date parsing

--- sources/stdlib/date/parse.js
+++ sources/stdlib/date/parse.js
@@ -5,6 +5,6 @@ export default function parse(next, context, { arg, options: { locale = `en-US` 
     let stdin = context.top();
     context = context.pop();
 
-    return next(context.push(moment(stdin, arg, locale, true).unix()));
+    return next(context.push(moment(stdin, arg, locale).unix()));
 
 }
